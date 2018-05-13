1e33616863c89439d15881a24e9f90cd4caa332c
Penguinniere <zaq12355@gmail.com>
2018-05-13 21:17:16 +0800

    intergate scripts and modify function.py

--- functional.py
+++ functional.py
@@ -5,6 +5,43 @@ from collections import defaultdict
 from copy import deepcopy
 from keras.models import load_model
 
+class GaussianNormalize(object):
+    def normalize(self, origin):
+        tmp = deepcopy(origin)
+        self.mean, self.std = {}, {}
+        for id, dateData in origin.items():
+            feature = np.array(list(dateData.values()), dtype=np.float32)
+            self.mean[id] = np.mean(feature, axis = 0)
+            self.std[id] = np.std(feature, axis = 0)
+            for date, feature in dateData.items():
+                tmp[id][date] = (feature - self.mean[id]) / self.std[id]
+        return tmp
+
+    def denormalize(self, adjusted):
+        if not self.mean or not self.std:
+            return adjusted
+        for id, data in adjusted.items():
+            adjusted[id] = data * self.std[id] + self.mean[id]
+        return adjusted
+
+class MaxNormalize(object):
+    def normalize(self, origin):
+        tmp = deepcopy(origin)
+        self.max = {}
+        for id, dateData in origin.items():
+            feature = np.array(list(dateData.values()), dtype=np.float32)
+            self.max[id] = np.max(feature, axis = 0)
+            for date, feature in dateData.items():
+                tmp[id][date] = feature / self.max[id]
+        return tmp
+
+    def denormalize(self, adjusted):
+        if not self.max:
+            return adjusted
+        for id, data in adjusted.items():
+            adjusted[id] = data * self.max[id]
+        return adjusted
+
 def parse_csv(filePath):
     temDict = defaultdict( lambda:defaultdict(lambda:list))
     with open(filePath, newline='',  encoding="big5-hkscs") as csvfile:
@@ -35,24 +72,6 @@ def write_csv(filePath, predDict):
             print(tmp)
             spamWriter.writerow(tmp)
 
-def preprocess( stockDict):
-    tmpStock = deepcopy(stockDict) 
-    tmpCoef = {}
-    for id, dateData in stockDict.items():
-        feature = np.array(list(dateData.values()), dtype=np.float32)
-        means = np.mean(feature, axis = 0)
-        stds = np.std(feature, axis = 0)
-        for date, feature in dateData.items():
-            tmpStock[id][date] = (feature - means) / stds
-        tmpCoef[id] = (means, stds)
-    return tmpStock, tmpCoef
-
-def restore_preprocess( predDict, adjustCoefs):
-    for id, predData in predDict.items():
-        predDict[id] = predData * adjustCoefs[id][1] + adjustCoefs[id][0]
-        predDict[id] = np.around(predDict[id], decimals=2)
-    return predDict
-
 def calculatePriceUpDown(npArray):
     price = npArray[-5:,-2]
     upDown = np.around(npArray[-5:,-2] * 100) - np.around(npArray[-6:-1,-2] * 100)
@@ -71,6 +90,16 @@ def score(realDict, predDict):
     print(score)
 
 def produce_pair( stockDict, postpone, days = 30):
+    tail = 30
+    feature, label = [], []
+    for id, dateData in stockDict.items():
+        data = np.vstack(dateData.values())
+        for i in range( data.shape[0]-days-tail-1):
+            feature.append( data[i:i+days])
+            label.append( data[i+days+postpone][3])
+    return np.array(feature), np.array(label)
+
+def new_produce_pair( stockDict, postpone, days = 30):
     tail = 30
     feature, label = [], []
     for id, dateData in stockDict.items():
---
+++ model.py
@@ -0,0 +1,62 @@
+from functional import *
+
+from optparse import OptionParser
+from keras.models import Model, load_model
+from keras.layers import GRU, Dense, Activation, Input
+from keras import optimizers, losses
+
+
+#fundBefore = parse_csv("TBrain_Round2_DataSet_20180331/tetfp.csv")
+#stockBefore = parse_csv("TBrain_Round2_DataSet_20180331/tsharep.csv")
+#stockAfter = parse_csv("TBrain_Round2_DataSet_20180331/tasharep.csv")
+
+if __name__ == '__main__':
+    optParser = OptionParser()
+    optParser.add_option("-t", "--train", action= "store_true", help="train")
+    optParser.add_option("-v", "--validate", action= "store_true", help="validate")
+    optParser.add_option("-p", "--predict", action= "store_true", help="predict")
+    (options, args) = optParser.parse_args()
+
+    fundAfter = parse_csv("TBrain_Round2_DataSet_20180511/taetfp.csv")
+    adjust = GaussianNormalize()
+    adjustStock = adjust.normalize(fundAfter)
+
+    if options.train:
+        for i in range(5):
+            feature, label = produce_pair(adjustStock, i)
+            #build model
+            inputs = Input(shape=(30, 5))
+            x = GRU(128, activation='relu')(inputs)
+            predictions = Dense(1, activation='linear')(x)
+
+            model = Model(inputs=inputs, outputs=predictions)
+            model.compile(optimizer='rmsprop',
+                    loss='mean_absolute_error')
+            model.fit( feature, label, epochs=300, batch_size=32)
+            model.save("ininII%d.h5"%(i))
+    
+    if options.validate or options.predict:
+        model = []    
+        result = defaultdict(list)
+        for i in range(5):
+            model.append(load_model("ininGG%d.h5"%(i)))
+
+        if options.validate:
+            for id, dateData in adjustStock.items():
+                data = list(dateData.values())[-35:-5]
+                data = np.array(data, dtype=np.float32, ndmin=3)
+                result[id].append(data[:,-1,-2])
+                for i in range(5):
+                    result[id].append(model[i].predict(data, batch_size=1)[0])
+            predDict = adjust.denormalize(result)
+            score(fundAfter, predDict)
+        
+        else:
+            for id, dateData in adjustStock.items():
+                data = list(dateData.values())[-30:]
+                data = np.array(data, dtype=np.float32, ndmin=3)
+                result[id].append(data[:,-1,-2])
+                for i in range(5):
+                    result[id].append(model[i].predict(data, batch_size=1)[0])
+            predDict = adjust.denormalize(result)
+            write_csv("result.csv", predDict)
