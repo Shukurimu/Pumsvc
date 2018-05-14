49d0d34484a8a87419492c57b3166bdf110d5e9c
Penguinniere <zaq12355@gmail.com>
2018-05-14 19:17:07 +0800

    finallly sucess

--- complexModel.py
+++ complexModel.py
@@ -7,6 +7,20 @@ from keras import optimizers, losses
 #fundBefore = parse_csv("TBrain_Round2_DataSet_20180331/tetfp.csv")
 #stockBefore = parse_csv("TBrain_Round2_DataSet_20180331/tsharep.csv")
 #stockAfter = parse_csv("TBrain_Round2_DataSet_20180331/tasharep.csv")
+def create_model_arch():
+    gruDim = 1
+    
+    inputs = Input(shape=(30, 5))
+    
+    gruTensor = GRU(5*gruDim)(inputs)
+    middle = []
+    for i in range(1, 6):
+        tmp = Lambda(lambda x: x[:,:gruDim*i], output_shape=(gruDim*i,))(gruTensor)
+        tmp = Dropout(0.5)(tmp) 
+        middle.append(Dense(1, activation='relu')(tmp))
+
+    outputs = Concatenate()(middle)
+    return Model(inputs=inputs, outputs=outputs)
 
 if __name__ == '__main__':
     optParser = OptionParser()
@@ -18,34 +32,21 @@ if __name__ == '__main__':
     fundAfter = parse_csv("TBrain_Round2_DataSet_20180511/taetfp.csv")
     adjust = MaxNormalize()
     adjustStock = adjust.normalize(fundAfter)
+    model = create_model_arch()
+    modelWeightName = "complexGRU.h5"
 
-    gruDim = 8
-    groupSize = list(a * gruDim for a in range(6))
-    
     if options.train:
         feature, label = new_produce_pair(adjustStock)
-
-        inputs = Input(shape=(30, 5))
-        gruTensor = GRU(5*gruDim)(inputs)
-
-        middle = []
-        for i in range(1, 6):
-            tmp = Lambda(lambda x: x[:,:groupSize[i]], output_shape=(groupSize[i],))(gruTensor)
-            tmp = Dropout(0.5)(tmp) 
-            middle.append(Dense(1, activation='relu')(tmp))
-        
-        outputs = Concatenate()(middle)
-        model = Model(inputs=inputs, outputs=outputs)
-
         model.compile(optimizer='rmsprop', loss='mean_squared_error')
         model.fit( feature, label, epochs=1, batch_size=32)
-        model.save("complexGRU.h5")
+        model.save_weights(modelWeightName)
 
     if options.validate or options.predict:
         result = {}
-        model = load_model("complexGRU.h5")
+        model.load_weights(modelWeightName)
 
         for val in [options.validate, options.predict*2]:
+            print(val)
             for id, dateData in adjustStock.items():
                 data = list(dateData.values())
                 data = data[-35:-5] if val < 2 else data[-30:]
